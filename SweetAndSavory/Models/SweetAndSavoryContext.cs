fb44adbe647160e5be9139832718c9818b0aabf2
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-25 16:24:45 -0700

    change name of folders

---
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndSavory.Models
+{
+    public class SweetAndSavoryContext : DbContext
+    {
+        public DbSet<Flavor> Flavors { get; set; }
+        public DbSet<Treat> Treats { get; set; }
+        public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+        public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
