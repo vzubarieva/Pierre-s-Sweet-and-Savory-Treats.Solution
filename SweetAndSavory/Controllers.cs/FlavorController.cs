b22ae40b6d3792fdfa44f347cd4b83a3d42a35b7
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-25 23:36:03 -0700

    add delete functionality for flavors

--- SweetAndSavory/Controllers.cs/FlavorController.cs
+++ SweetAndSavory/Controllers.cs/FlavorController.cs
@@ -93,23 +93,28 @@ namespace SweetAndSavory.Controllers
             return RedirectToAction("Index");
         }
 
-        //         public ActionResult Delete(int id)
-        //         {
-        //             var thisRestaurant = _db.Restaurants.FirstOrDefault(
-        //                 restaurant => restaurant.RestaurantId == id
-        //             );
-        //             return View(thisRestaurant);
-        //         }
+        public ActionResult Delete(int id)
+        {
+            var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            return View(thisFlavor);
+        }
 
-        //         [HttpPost, ActionName("Delete")]
-        //         public ActionResult DeleteConfirmed(int id)
-        //         {
-        //             var thisRestaurant = _db.Restaurants.FirstOrDefault(
-        //                 restaurant => restaurant.RestaurantId == id
-        //             );
-        //             _db.Restaurants.Remove(thisRestaurant);
-        //             _db.SaveChanges();
-        //             return RedirectToAction("Index");
-        //         }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+            _db.Flavors.Remove(thisFlavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFlavor(int joinId)
+        {
+            var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+            _db.FlavorTreat.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
