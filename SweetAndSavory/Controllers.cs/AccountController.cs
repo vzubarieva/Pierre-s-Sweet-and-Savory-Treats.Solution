84755b8632b89acdb5c89d22c2d3f84b258fbaa5
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-08-27 10:34:30 -0700

    add views for registering

---
+++ SweetAndSavory/Controllers.cs/AccountController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SweetAndSavory.Models;
+using System.Threading.Tasks;
+using SweetAndSavory.ViewModels;
+
+namespace ToDoList.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SweetAndSavoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            SweetAndSavoryContext db
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
